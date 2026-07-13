d24aff522505d595a495eb9d49af981731b96630
Rui Pan 潘瑞 <rpan0726@gmail.com>
2026-07-13 13:41:44 -0400

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.py
@@ -0,0 +1,21 @@
+import heapq
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        max_heap = []
+        heapq.heapify(max_heap)
+
+        for s in stones:
+            heapq.heappush(max_heap, -s)
+        
+        while len(max_heap) >= 2:
+            x = -heapq.heappop(max_heap)
+            y = -heapq.heappop(max_heap)
+
+            # print(f"Popped )
+
+            if x > y:
+                heapq.heappush(max_heap, -(x - y))
+
+
+        return -max_heap[0] if max_heap else 0
